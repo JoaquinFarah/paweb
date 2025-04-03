09b6f4208e50c00a571573247d66449ae4355662
JFarah <joaquinefarah@gmail.com>
2025-04-03 13:06:14 -0300

    update Navbar styles with gradient background and add new Misión link

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
 
   return (
     // style={{ backgroundColor: '#e5771b' }}
-    <nav style={{ backgroundColor: '#e5771b' }} className="shadow-md fixed w-full z-50">
+    <nav style={{ backgroundColor: '#e5771b' }} className="bg-gradient-to-br from-20% to-white shadow-md fixed w-full z-50">
       <div className="w-full mx-0 px-1 sm:px-1 lg:px-2">
         <div className="flex justify-between items-center h-16">
           
@@ -27,28 +27,29 @@ const Navbar: React.FC = () => {
           
           {/* Links rightside */}
           <div className="flex items-center justify-end space-x-6 mr-52">
-
-            <Link href="/pages/servicepage" className="no-underline decoration-2 transition-all duration-200 hover:underline hover:decoration-blue-400 text-xl" style={{ textShadow: '2px 2px 4px rgba(0, 191, 255, 1)' }}>
-            Sombreado
-            </Link>     
           
-            <Link href="/pages/about" className="no-underline decoration-2 transition-all duration-200 hover:underline hover:decoration-blue-400">
+            <Link href="/pages/about" className="no-underline decoration-2 transition-all duration-200 hover:underline hover:decoration-blue-400" style={{ textShadow: '2px 2px 4px #e5771b' }}>
             Nosotros
             </Link>
 
-            <Link href="/pages/servicepage" className="no-underline decoration-2 transition-all duration-200 hover:underline hover:decoration-blue-400">
+            <Link href="/pages/servicepage" className="no-underline decoration-2 transition-all duration-200 hover:underline hover:decoration-blue-400" style={{ textShadow: '2px 2px 4px #e5771b' }}>
             Servicios
             </Link>
 
-            <Link href="/pages/productpage" className="no-underline decoration-2 transition-all duration-200 hover:underline hover:decoration-blue-400">
+            <Link href="/pages/productpage" className="no-underline decoration-2 transition-all duration-200 hover:underline hover:decoration-blue-400" style={{ textShadow: '2px 2px 4px #e5771b' }}>
             Visión
             </Link>
+
+            <Link href="/pages/servicepage" className="no-underline decoration-2 transition-all duration-200 hover:underline hover:decoration-blue-400" style={{ textShadow: '2px 2px 4px #e5771b' }}>
+            Misión
+            </Link>  
 
           </div>
         </div>
       </div>
-      <div className="h-0.5 bg-gradient-to-r from-white via-amber-400 to-white"></div>
-      {/* <div className="h-1 bg-gradient-to-r from-green-800 via-white to-red-800"></div> */}
+      {/* <div className="h-0.5 bg-gradient-to-r from-amber-600 via-white to-amber-600"></div>
+      <div className="h-0.5 bg-gradient-to-r from-white via-amber-600 to-white"></div> */}
+      
     </nav>
   );
 };
