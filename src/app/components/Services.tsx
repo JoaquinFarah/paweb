019d532585e686fdb63de979404b88331c9a3327
JFarah <joaquinefarah@gmail.com>
2025-04-03 17:02:24 -0300

    update Services component with new content and images for enhanced service offerings

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -1,34 +1,63 @@
 import React from 'react';
-
 import Image from 'next/image';
 
 const callout = [   
   {
-    name: 'Innovacion y tecnologia',      
-    description: 'La tecnología de nuestros productos es innovadora y de alta calidad, lo que garantiza su eficacia y durabilidad. Utilizamos materiales de última generación y técnicas avanzadas para ofrecer soluciones efectivas y confiables.',
-    imageSrc: '/escalera.jpg',
+    name: 'Venta y alquiler de Equipos de Altura',      
+    description: 'Nuestros equipamientoss de altura son ideales para trabajos en espacios confinados, como túneles y pozos. Ofrecemos soluciones personalizadas para cada necesidad, garantizando la seguridad y eficiencia en el trabajo.',
+    imageSrc: '/altura.jpg',
     imageAlt: 'serv1',
     width: 1000,
-    height: 1000,
-    
+    height: 1000,    
   }, 
   {
-    name: 'Capacitaciones',
+    name: 'Capacitación Protección en Altura Con Accesorios y elementos de X-URBAN',
     description: 'Capacitamos a los brigadistas en el uso de nuestros productos y brindamos asesoramiento técnico para garantizar su correcto funcionamiento. Nuestro equipo de expertos está disponible para resolver cualquier duda o inquietud que puedan tener nuestros clientes.',
-    imageSrc: '/tunel1.jpg',
+    imageSrc: '/xurban.jpg',
     imageAlt: 'serv2',
     width: 1000,
     height: 1000,
   },  
   {
-    name: 'Venta y alquiler de equipos',
-    description: 'Ofrecemos una amplia gama de productos para la seguridad laboral, incluyendo equipos de protección personal, sistemas de rescate y evacuación, y herramientas de trabajo en altura. Nuestros productos están diseñados para cumplir con los más altos estándares de calidad y seguridad.',    
+    name: 'Capacitación en Espacios Confinados',
+    description: 'Ofrecemos capacitación especializada en espacios confinados, brindando a los brigadistas las herramientas necesarias para trabajar de manera segura y eficiente. Nuestros cursos incluyen teoría y práctica, asegurando que los participantes adquieran los conocimientos necesarios para enfrentar situaciones de riesgo.',    
     imageSrc: '/bajando.jpg',
     imageAlt: 'serv3',
     width: 1000,
     height: 1000,
-    href: '/pages/news/uncuyo',
   },  
+  {
+    name: 'Capacitación sobre uso de Equipos Autónomos',      
+    description: 'En situaciones donde son requeridos equipos autónomos, como en espacios confinados, ofrecemos capacitación especializada para el uso de estos equipos. Nuestros cursos incluyen teoría y práctica, asegurando que los participantes adquieran los conocimientos necesarios para enfrentar situaciones de riesgo.',
+    imageSrc: '/espaciosconfinados2.jpg',
+    imageAlt: 'serv1',
+    width: 1000,
+    height: 1000,    
+  }, 
+  {
+    name: 'Mantenimiento preventivo y prueba hidráulica de cilindros E.R.A.',      
+    description: 'Contamos con especialistas de mantenimiento de cilindros E.R.A. para garantizar el correcto funcionamiento de los equipos. Realizamos pruebas hidráulicas y mantenimiento preventivo, asegurando la seguridad y eficiencia en el trabajo.',
+    imageSrc: '/hidraulica.jpg',
+    imageAlt: 'serv1',
+    width: 1000,
+    height: 1000,    
+  }, 
+  {
+    name: 'Alquiler de equipos detectores de Gases personales',      
+    description: 'Cuando los brigadistas deben trabajar en espacios confinados, ofrecemos equipos detectores de gases personales para garantizar la seguridad de los trabajadores. Nuestros equipos son de última tecnología y cumplen con los estándares de seguridad requeridos.',
+    imageSrc: '/detector.webp',
+    imageAlt: 'serv1',
+    width: 1000,
+    height: 1000,    
+  }, 
+  {
+    name: 'Calibración y Mantenimiento preventivo de Equipos detector de gas',      
+    description: 'Siempre al dia con los últimos avances en tecnología de detección de gases, ofrecemos calibración y mantenimiento preventivo de equipos detectores de gas. Nuestros técnicos especializados garantizan el correcto funcionamiento de los equipos, asegurando la seguridad de los trabajadores.',
+    imageSrc: '/calibracion.jpg',
+    imageAlt: 'serv1',
+    width: 1000,
+    height: 1000,    
+  }, 
   ]
   
   export default function NewsSection() {
@@ -49,10 +78,10 @@ const callout = [
                     />
                   </div>
                   <h3 className="mt-4 text-2xl text-indigo-500 items-center text-center font-semibold leading-6">
-                    <a href={callout.href}>
+                    <p>
                       <span className="absolute inset-0" />
                       {callout.name}
-                    </a>
+                    </p>
                   </h3>
                   <p className="text-base font-semibold text-gray-900 mb-6 text-left px-4 mt-10">{callout.description}</p>
                 </div>
