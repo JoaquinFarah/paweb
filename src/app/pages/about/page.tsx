555174f32ec2c79dde8f1034b4120f7054d87025
JFarah <joaquinefarah@gmail.com>
2025-04-07 16:51:16 -0300

    Enhance About page image styling with shadow for improved visual appeal

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -16,7 +16,7 @@ export default function AboutPage() {
           alt="Icono" 
           width={2000} 
           height={2000} 
-          className="absolute top-0 left-0 w-full h-full object-cover" 
+          className="absolute top-0 left-0 w-full h-full object-cover shadow-xl" 
         />
         <div className="absolute top-1/3 left-1/5 transform -translate-x-1/7 -translate-y-1 text-white text-5xl font-semibold text-center rounded-lg bg-gray-900/50 p-5 backdrop-blur-sm">
           <p className="uppercase">Impulsando el Éxito de tu Proyecto con Soluciones Prácticas</p>
